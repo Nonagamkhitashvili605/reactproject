a87db36df41071df868f5136fdeb11c13d8e160c
nonagamkhitashvili605 <nonagamkhitashvili@gmail.com>
2025-09-29 19:55:19 +0400

    add 23.09 classwork

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,50 @@
 import React, { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 // import Post from "./Post";
 // import { data } from "./data";
-import Modal from "./components/Modal";
-import Profile from "./components/Profile";
+// import Modal from "./components/Modal";
+// import Profile from "./components/Profile";
 // import Greeting from "./assets/Greeting";
-import Header from "./components/Header";
-import Card from "./components/Card";
-import Helloreact from "./components/Helloreact";
-import Satauri from "./components/Satauri";
-import Greeting from "./components/Greeting";
-import User from "./components/User";
-
-const arr = [
-  { name: "nona", description: "lorem," },
-  { name: "nona", description: "lorem," },
-  { name: "nona", description: "lorem," },
-  { name: "nona", description: "lorem," },
-  { name: "nona", description: "lorem," },
-  { name: "nona", description: "lorem," },
-  { name: "nona", description: "lorem," },
-  { name: "nona", description: "lorem," },
-  { name: "nona", description: "lorem," },
-  { name: "nona", description: "lorem," },
-];
+// import Header from "./components/Header";
+// import Card from "./components/Card";
+// import Helloreact from "./components/Helloreact";
+// import Satauri from "./components/Satauri";
+// import Greeting from "./components/Greeting";
+// import User from "./components/User";
+import State from "./components/state/State";
+// const arr = [
+//   { name: "nona", description: "lorem," },
+//   { name: "nona", description: "lorem," },
+//   { name: "nona", description: "lorem," },
+//   { name: "nona", description: "lorem," },
+//   { name: "nona", description: "lorem," },
+//   { name: "nona", description: "lorem," },
+//   { name: "nona", description: "lorem," },
+//   { name: "nona", description: "lorem," },
+//   { name: "nona", description: "lorem," },
+//   { name: "nona", description: "lorem," },
+// ];
 
 function App() {
-  const onclick = () => {
-    console.log("hi");
-  };
-  const type = "list";
-  const arr = ["name", "lastname", "name", "lastname", "name", "lastname"];
-  const arrObj = [
-    { name: "nona", age: 35 },
-    { name: "eka", age: 20 },
-    { name: "giorgi", age: 7 },
-  ];
+  // const onclick = () => {
+  //   console.log("hi");
+  // };
+  // const type = "list";
+  // const arr = ["name", "lastname", "name", "lastname", "name", "lastname"];
+  // const arrObj = [
+  //   { name: "nona", age: 35 },
+  //   { name: "eka", age: 20 },
+  //   { name: "giorgi", age: 7 },
+  // ];
   return (
     <div>
-      <Header />
+      {/* <Header />
       <Helloreact />
       <Satauri />
       <Greeting />
-      <User />
+      <User /> */}
 
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {/* <div style={{ display: "flex", flexWrap: "wrap" }}>
         <Card />
         <Card />
         <Card />
@@ -55,23 +53,23 @@ function App() {
         <Card />
         <Card />
         <Card />
-      </div>
+      </div> */}
       <>
-        {arr.map(({ name, description }) => (
+        {/* {arr.map(({ name, description }) => (
           <div style={{ border: "1px solid red", margin: "10px" }}>
             <h1>{name}</h1>
             <p>{description}</p>
           </div>
-        ))}
+        ))} */}
       </>
       {/* <Profile isAdmin={true} /> */}
       {/* <Modal header="title" footer="footer">
         <p>text</p>
       </Modal> */}
-      <User name="ნინო" />
-      <User name="მარი" />
+      {/* <User name="ნინო" />
+      <User name="მარი" /> */}
 
-      <h1>nona</h1>
+      {/* <h1>nona</h1>
       <button onclick={onclick}>click </button>
       <p>{type} hello react</p>
       {arr.map((item, index) => (
@@ -81,7 +79,9 @@ function App() {
         <h3 key={index}>
           {name}-{age}
         </h3>
-      ))}
+      ))} */}
+
+      <State />
     </div>
   );
 }
---
+++ src/components/state/State.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+// import { Data } from "./data";
+import { data } from "./../../data";
+const State = () => {
+  const [state, setState] = useState(5);
+  const [boolean, setBoolean] = useState(true);
+  const [info, setInfo] = useState(data);
+  const [infotext, setInfotext] = useState("");
+  const [InfoEmail, setInfoEmail] = useState("");
+  const [isClicked, setIsClicked] = useState("false");
+
+  const click = () => {
+    // setState("hhhhh");
+    // state == 5 ? setState("ndhdh") : setState(5);
+    // setBoolean(!boolean);
+    setInfo(
+      info.filter((item) => {
+        return item.id % 2 == 0;
+      })
+    );
+  };
+
+  const onClick = (e) => {
+    setInfotext(e.target.value);
+  };
+  const emailChange = (e) => {
+    setInfoEmail(e.target.value);
+  };
+
+  const button = () => {
+    setIsClicked(true);
+  };
+  return (
+    <div>
+      {state}
+      <button onClick={click}>click</button>
+      {boolean == true ? <p>ჭეშმარიტი</p> : <p>მცდარი</p>}
+      <button onClick={click}>filter</button>
+      {info.map((item, key) => (
+        <p key={key}>{item.title}</p>
+      ))}
+      <button onClick={button}>click</button>
+      {isClicked && (
+        <p>
+          {infotext}---------- {InfoEmail}{" "}
+        </p>
+      )}
+      <form>
+        <input
+          type="text"
+          onChange={onClick}
+          placeholder="write some text"
+          value={infotext}
+        />
+        <input
+          type="email"
+          onChange={emailChange}
+          placeholder="write some email"
+          value={InfoEmail}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default State;
